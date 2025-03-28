4104df8e151570e951326c23e0dcea074c9f3337
Masaya Iwasa <miwasa@c6r1s1.42tokyo.jp>
2025-03-28 22:05:32 +0900

    [fix]

--- ex01/Serializer.cpp
+++ ex01/Serializer.cpp
@@ -6,11 +6,40 @@
 /*   By: miwasa <miwasa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 21:02:57 by miwasa            #+#    #+#             */
-/*   Updated: 2025/03/11 21:03:19 by miwasa           ###   ########.fr       */
+/*   Updated: 2025/03/28 21:50:17 by miwasa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/25 00:26:10 by hshimizu          #+#    #+#             */
+/*   Updated: 2024/08/25 00:49:51 by hshimizu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Serializer.hpp"
+
+Serializer::Serializer() {
+}
+
+Serializer::~Serializer() {
+}
+
+Serializer::Serializer(Serializer const &other) {
+	(void)other;
+}
+
+Serializer &Serializer::operator=(Serializer const &other) {
+	(void)other;
+	return *this;
+}
 
 uintptr_t Serializer::serialize(Data* ptr) {
 	return reinterpret_cast<uintptr_t>(ptr);
--- ex01/Serializer.hpp
+++ ex01/Serializer.hpp
@@ -6,7 +6,7 @@
 /*   By: miwasa <miwasa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 21:01:18 by miwasa            #+#    #+#             */
-/*   Updated: 2025/03/11 21:14:33 by miwasa           ###   ########.fr       */
+/*   Updated: 2025/03/28 21:48:06 by miwasa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ private:
 	Serializer();
 	Serializer(const Serializer&);
 	Serializer& operator=(const Serializer&);
+	~Serializer();
 public:
 	static uintptr_t serialize(Data* ptr);
 	static Data* deserialize(uintptr_t raw);
